d45f5aa143b6617f3522d3edace4642d509a3889
NelliWojo <120693666+Raftouch@users.noreply.github.com>
2024-06-12 15:46:30 +0200

    add basic form

--- src/components/BasicForm.tsx
+++ src/components/BasicForm.tsx
@@ -1,8 +1,60 @@
+import { FormikHelpers, useFormik } from "formik";
+
+interface FormValues {
+  email: string;
+  age: number;
+  password: string;
+  confirmPassword: string;
+}
+
 export default function BasicForm() {
+  const {values, handleChange, handleBlur} = useFormik<FormValues>({
+    initialValues: {
+      email: "",
+      age: 0,
+      password: "",
+      confirmPassword: "",
+    },
+    onSubmit: (
+      values: FormValues,
+      { setSubmitting }: FormikHelpers<FormValues>
+    ) => {
+      console.log(values);
+      setSubmitting(false);
+    },
+  });
+    console.log(values)
+
   return (
     <form autoComplete="off">
       <label htmlFor="email">Email</label>
-      <input id="email" type="email" placeholder="Enter your email" />
+      <input
+        id="email"
+        type="email"
+        placeholder="Enter your email"
+        value={values.email}
+        onChange={handleChange}
+        onBlur={handleBlur}
+      />
+      <label htmlFor="age">Age</label>
+      <input
+        id="age"
+        type="number"
+        placeholder="Enter your age"
+        value={values.age}
+        onChange={handleChange}
+        onBlur={handleBlur}
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        type="paswword"
+        placeholder="Enter your password"
+        value={values.password}
+        onChange={handleChange}
+        onBlur={handleBlur}
+      />
+      <button type="submit">Submit</button>
     </form>
   );
 }
